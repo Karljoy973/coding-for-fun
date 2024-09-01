b7fe94b75228c3513d2de4b9feeea741dcfc08d4
973Karljoy <karljoyeux@gmail.com>
2024-09-01 11:57:37 +0200

    Organizing the code in mvc step1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
+// const style = require("./public/style.css");
+
+//cadrant circulaire
+let baseCadrantClass = "ui-component";
+let Cadrant = document.createElement("div");
+Cadrant.setAttribute("class", baseCadrantClass);
+
+// zone lumineuse
+let baseLightAreaClass = "ui-component";
+let lightArea = document.createElement("div");
+lightArea.setAttribute("class", baseLightAreaClass);
+
+// boutton lumière
+let baseLightButtonClass = "ui-component";
+let ligthButton = document.createElement("div");
+ligthButton.setAttribute("class", baseLightButtonClass);
+
+// boutton heures
+let baseHourButtonClass = "ui-component";
+let hourButton = document.createElement("div");
+hourButton.setAttribute("class", baseHourButtonClass);
+
+// hour Element
+let baseHourElementClass = "ui-component";
+let hourElement = document.createElement("p");
+hourElement.setAttribute("class", baseHourElementClass);
+hourElement.innerText = "I am clock";
+
+//boutton settings
+let baseSettingsButtonClass = "ui-component";
+let buttonSettings = document.createElement("div");
+buttonSettings.setAttribute("class", baseSettingsButtonClass);
+
+let cadrantChildren = [lightArea, buttonSettings, hourButton, ligthButton];
+let lightChildren = [hourElement];
+let rootChildren = [Cadrant];
+//composition
+
+lightChildren.forEach((e: HTMLDivElement) => lightArea.appendChild(e));
+cadrantChildren.forEach((e: HTMLDivElement) => Cadrant.appendChild(e));
+rootChildren.forEach((e) => document.body.appendChild(e));
 console.log("this is clock");
--- webpack.config.js
+++ webpack.config.js
@@ -9,37 +9,62 @@ module.exports = {
 		rules: [
 			{
 				test: /\.tsx?$/,
-				use: "ts-loader",
+				use: {
+					loader: "ts-loader",
+					options: {
+						transpileOnly: true, // Skip type-checking in development mode
+						happyPackMode: true, // Enable HappyPack mode for faster builds
+					},
+				},
 				exclude: /node_modules/,
 			},
 			{
 				test: /\.css$/i,
 				use: ["style-loader", "css-loader"],
 			},
+			// {
+			// 	loader: "css-loader",
+			// 	options: {
+			// 		modules: true,
+			// 	},
+			// },
 		],
+	},
+	cache: {
+		type: "filesystem", // Enables persistent caching
 	},
 	devServer: {
 		host: "0.0.0.0",
 
-		port: 4001, //port that we're using for local host (localhost:8080)
+		port: 4005, //port that we're using for local host (localhost:8080)
 
 		static: path.resolve(appDirectory, "./public"), //tells webpack to serve from the public folder
-
+		compress: true,
+		client: { overlay: true },
 		hot: true,
 
 		server: { type: "https" },
 	},
 	resolve: {
-		extensions: [".tsx", ".ts", ".js"],
+		extensions: [".ts", ".js"],
 	},
 	output: {
 		filename: "bundle.js",
-		path: path.resolve(__dirname, "dist"),
+		path: path.resolve(__dirname, "public"),
+	},
+	performance: {
+		hints: false,
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			inject: true,
 			template: path.resolve(appDirectory, "./public/index.html"),
+			chunks: "all",
+			chunksSortMode: "auto",
+			favicon: undefined,
+			minify: "auto",
 		}),
 	],
+	mode: "development",
+	devtool: process.env.NODE_ENV === "production" ? false : "source-map",
 };
